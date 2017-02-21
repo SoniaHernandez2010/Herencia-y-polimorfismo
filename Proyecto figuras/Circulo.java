4bbeb9da52de287d036b7299e6d1998c955fda30
SoniaHernandez2010 <soniahdzhu@gmail.com>
2017-02-20 23:27:27 -0600

    Clase Cuadrado y Circulo

---
+++ Proyecto figuras/Circulo.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Circulo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Circulo extends Figura
+{
+    private float radio;
+    
+    public Circulo(float r)
+    {
+        radio = r;
+    }
+    
+    public void calculaArea()
+    {
+        area = 3.14 * radio;
+    }
+}
+
