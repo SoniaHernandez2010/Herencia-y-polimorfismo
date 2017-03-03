53163a86a25ca5e1150e721045257dfcca1ce9fa
123 <feral1592@gmail.com>
2017-03-03 11:56:00 -0600

    Ejercicio promedios(02-Marzo-17)

---
+++ Ejercicio Promedios/Banco.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Banco
+{
+    private ArrayList<CuentaBancaria> cuentas;
+    
+    public Banco()
+    {
+        cuentas = new ArrayList<CuentaBancaria> ();
+    }
+    
+    public agregaCuenta(CuentaBancaria c)
+    {
+        for(CuentaBancaria 
+}
---
+++ Ejercicio Promedios/CuentaBancaria.java
@@ -0,0 +1,6 @@
+
+
+public class CuentaBancaria
+{
+    private int saldo;
+}
